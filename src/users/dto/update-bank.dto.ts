9924fc893f626f995fe10a6a82e2449f10195b73
shalomagunbiade8-del <shalomagunbiade8@gmail.com>
2026-06-09 10:28:43 +0100

    up b details

--- src/users/dto/update-bank.dto.ts
+++ src/users/dto/update-bank.dto.ts
@@ -1,5 +1,20 @@
+import {
+  IsString,
+  IsNotEmpty,
+} from 'class-validator';
+
 export class UpdateBankDto {
+
+  @IsString()
+  @IsNotEmpty()
   bankName: string;
+
+  @IsString()
+  @IsNotEmpty()
   accountNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
   accountName: string;
-} 
+
+}
